2e9bc0037529ff83b024306849c5006f05f4de1a
SW ENG <sweng@massload.com>
2024-04-12 16:49:48 -0600

    update controller to remodel staic modelname added

--- src/NewTemplateManager.Api/Controllers/V1/ModelsController.cs
+++ src/NewTemplateManager.Api/Controllers/V1/ModelsController.cs
@@ -1,28 +1,25 @@
-﻿using Asp.Versioning;
-using NewTemplateManager.Api.Extensions;
-using NewTemplateManager.Application.CQRS.Model.Commands;
-using NewTemplateManager.Application.CQRS.Model.Queries;
+﻿using NewTemplateManager.Api.Extensions;
+using NewTemplateManager.Application.CQRS;
+using Asp.Versioning;
 using NewTemplateManager.Contracts.RequestDTO.V1;
 using NewTemplateManager.Contracts.ResponseDTO.V1;
 using MediatR;
+using NewTemplateManager.Api.Controllers;
 using Microsoft.AspNetCore.Mvc;
-namespace NewTemplateManager.Api.Controllers.v1
+using NewTemplateManager.Application.CQRS.Model.Commands;
+using NewTemplateManager.Application.CQRS.Model.Queries;
+namespace NewTemplateManager.Api.Controllers.V1
 {
     [ApiVersion(1)]
     public class ModelsController : TheBaseController<ModelsController>
     {
-
 
         public ModelsController(ILogger<ModelsController> logger, ISender sender) : base(logger, sender) { }
 
         [ProducesResponseType(typeof(IEnumerable<ModelResponseDTO>), StatusCodes.Status200OK)]
         [HttpGet(template: NewTemplateManagerAPIEndPoints.Model.Get, Name = NewTemplateManagerAPIEndPoints.Model.Get)]
         public Task<IActionResult> Get(CancellationToken cToken) => _sender.Send(new GetAllModelQuery(), cToken).ToActionResult();
-        //public IActionResult Get(CancellationToken cToken)
-        //{
-           
-        //  return new  OkObjectResult(GeneralFailures.DataNotFoundInRepository);
-        //}
+
         [ProducesResponseType(typeof(ModelResponseDTO), StatusCodes.Status200OK)]
         [HttpGet(template: NewTemplateManagerAPIEndPoints.Model.GetById, Name = NewTemplateManagerAPIEndPoints.Model.GetById)]
         public Task<IActionResult> GetById([FromRoute] string NameOrGuid, CancellationToken cancellationToken)
@@ -31,20 +28,16 @@ namespace NewTemplateManager.Api.Controllers.v1
                 (_sender.Send(new GetModelByGuidQuery(new ModelGetRequestByGuidDTO(guid)), cancellationToken)).ToEitherActionResult()
                 :
                 (_sender.Send(new GetModelByIdQuery(new ModelGetRequestByIdDTO(NameOrGuid)), cancellationToken)).ToEitherActionResult();
-
-
         }
 
-        [ProducesResponseType(typeof(ModelTypeResponseDTO), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ModelResponseDTO), StatusCodes.Status200OK)]
         [HttpGet(template: NewTemplateManagerAPIEndPoints.Model.GetByJSONBody, Name = NewTemplateManagerAPIEndPoints.Model.GetByJSONBody)]
-
         public Task<IActionResult> GetByJSONBody([FromBody] ModelGetRequestDTO request, CancellationToken cancellationToken)
                 => (_sender.Send(new GetModelQuery(request), cancellationToken)).ToEitherActionResult();
 
         [HttpPost(template: NewTemplateManagerAPIEndPoints.Model.Create, Name = NewTemplateManagerAPIEndPoints.Model.Create)]
         public Task<IActionResult> Create(ModelCreateRequestDTO request, CancellationToken cancellationToken)
              => (_sender.Send(new CreateModelCommand(request), cancellationToken)).ToActionResultCreated($"{NewTemplateManagerAPIEndPoints.Model.Create}", request);
-
 
         [HttpPut(template: NewTemplateManagerAPIEndPoints.Model.Update, Name = NewTemplateManagerAPIEndPoints.Model.Update)]
         public Task<IActionResult> Update(ModelUpdateRequestDTO request, CancellationToken cancellationToken)
